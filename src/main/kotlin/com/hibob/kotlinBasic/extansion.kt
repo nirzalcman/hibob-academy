2eaa9096266bf9fd1bc0a76a81d47fa15c96c3b5
Nir Zalcman <nir.zalcman@hibob.io>
2024-09-03 10:55:57 +0300

    Extension assignment

--- src/main/kotlin/com/hibob/kotlinBasic/extansion.kt
+++ src/main/kotlin/com/hibob/kotlinBasic/extansion.kt
@@ -1,6 +1,16 @@
 package com.hibob.kotlinBasic
 
-class extansion {
+fun List<Int>.sumNew() : Int {
+    var sum  = 0
+    for (number in this) sum+=number
+    return sum
+}
 
+infix fun Number.toPowerOf(exponent: Number): Double{
+    return Math.pow(this.toDouble() , exponent.toDouble())
+}
 
+fun main(args: Array<String>) {
+    println(listOf(1, 2, 3, 4, 5).sumNew())
+    println(5.5.toPowerOf(2))
 }
