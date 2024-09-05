d55c3e80be9d1657805ce5d3844295de3b8b3468
Nir Zalcman <nir.zalcman@hibob.io>
2024-09-05 11:53:51 +0300

    Types and Properties - complete to merge

--- src/main/kotlin/com/hibob/kotlinBasic/StoreService.kt
+++ src/main/kotlin/com/hibob/kotlinBasic/StoreService.kt
@@ -8,7 +8,6 @@ import java.time.LocalDateTime
 class StoreService {
 
     fun pay(cart: List<Cart>, payment: Payment): Map<String, Check> {
-        /*return cart.map { it -> it.clientId to checkout(it, payment) }.toMap()*/
         return cart.associate { it -> it.clientId to checkout(it, payment) }
     }
 
@@ -19,16 +18,6 @@ class StoreService {
         return Check(cart.clientId, Statuses.FAILURE, 0.0)
     }
 
-    /*
-        fun checkPayment (payment: Payment ,price : Double): Boolean {
-        return when(payment){
-            is Payment.Cash-> fail("you cant pay with cash")
-            is Payment.PayPal-> '@' in payment.email
-            is Payment.CreditCard-> (payment.type == CreditCardType.MASTERCARD || payment.type == CreditCardType.VISA ) && (payment.expiryDate.isAfter(LocalDate.now())) &&(price<payment.limit)&&(payment.number.length==10)
-        }
-        }
-
-     */
 
     fun checkPayment(payment: Payment, price: Double): Boolean {
         return when (payment) {
@@ -41,13 +30,13 @@ class StoreService {
     private fun validateCreditCard(payment: Payment.CreditCard, price: Double) =
         isSupportedType(payment.type) && isCardDetailsValid(payment) && isNotOverTheLimit(price, payment)
 
-    private fun isSupportedType(cardType: CreditCardType) =
+    private fun isSupportedType(cardType: CreditCardType): Boolean =
         (cardType == CreditCardType.MASTERCARD || cardType == CreditCardType.VISA)
 
-    private fun isCardDetailsValid(payment: Payment.CreditCard) =
+    private fun isCardDetailsValid(payment: Payment.CreditCard): Boolean =
         payment.expiryDate.isAfter(LocalDate.now()) && payment.number.length == 10
 
-    private fun isNotOverTheLimit(price: Double, payment: Payment.CreditCard) = price < payment.limit
+    private fun isNotOverTheLimit(price: Double, payment: Payment.CreditCard): Boolean = price < payment.limit
 
     fun check(obj: Any): Boolean {
         return obj is Boolean && obj == true
