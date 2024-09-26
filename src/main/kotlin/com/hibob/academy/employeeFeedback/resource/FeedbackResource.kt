def43973d8d2700aeb8aec0b84a7e2f61ab4f162
Nir Zalcman <nir.zalcman@hibob.io>
2024-09-26 15:06:01 +0300

    add feedback resource

--- src/main/kotlin/com/hibob/academy/employeeFeedback/resource/FeedbackResource.kt
+++ src/main/kotlin/com/hibob/academy/employeeFeedback/resource/FeedbackResource.kt
@@ -2,12 +2,10 @@ package com.hibob.academy.employeeFeedback.resource
 
 import com.hibob.academy.employeeFeedback.Service.FeedbackCreator
 import com.hibob.academy.employeeFeedback.Service.FeedbackFetcher
+import com.hibob.academy.employeeFeedback.Service.FeedbackResponseCreator
 import com.hibob.academy.employeeFeedback.Service.FeedbackUpdater
 import com.hibob.academy.employeeFeedback.annotations.AllowedRoles
-import com.hibob.academy.employeeFeedback.dao.CreationFeedback
-import com.hibob.academy.employeeFeedback.dao.FeedbackFilter
-import com.hibob.academy.employeeFeedback.dao.Role
-import com.hibob.academy.employeeFeedback.dao.UserLoggedInDetails
+import com.hibob.academy.employeeFeedback.dao.*
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.ws.rs.*
 import jakarta.ws.rs.core.MediaType
@@ -20,16 +18,22 @@ import org.springframework.web.bind.annotation.RequestBody
 @Produces(MediaType.APPLICATION_JSON)
 @Path("/api/feedback")
 
-class FeedbackResource (private val feedbackCreator: FeedbackCreator , private val feedbackFetcher: FeedbackFetcher, private val feedbackUpdater: FeedbackUpdater,private val request: HttpServletRequest) {
+class FeedbackResource(
+    private val feedbackCreator: FeedbackCreator,
+    private val feedbackFetcher: FeedbackFetcher,
+    private val feedbackUpdater: FeedbackUpdater,
+    private val feedbackResponseCreator: FeedbackResponseCreator,
+    private val request: HttpServletRequest
+) {
 
     @POST
     fun createFeedback(@RequestBody creationFeedback: CreationFeedback): Response {
         val userLoggedInDetails = getUserDetails()
-        return Response.ok(feedbackCreator.createFeedBack(userLoggedInDetails,creationFeedback)).build()
+        return Response.ok(feedbackCreator.createFeedBack(userLoggedInDetails, creationFeedback)).build()
     }
 
     @GET
-    @AllowedRoles(Role.ADMIN,Role.HR)
+    @AllowedRoles(Role.ADMIN, Role.HR)
     fun getAllFeedbacks(): Response {
         val userLoggedInDetails = getUserDetails()
         return Response.ok(feedbackFetcher.getAllFeedbacks(userLoggedInDetails.companyId)).build()
@@ -37,36 +41,48 @@ class FeedbackResource (private val feedbackCreator: FeedbackCreator , private v
 
     @GET
     @Path("/{id}")
-    @AllowedRoles(Role.ADMIN,Role.HR)
+    @AllowedRoles(Role.ADMIN, Role.HR)
     fun getFeedbackById(@PathParam("id") feedbackId: Long): Response {
         val userLoggedInDetails = getUserDetails()
-        return Response.ok(feedbackFetcher.getFeedbackById(userLoggedInDetails.companyId,feedbackId)).build()
+        return Response.ok(feedbackFetcher.getFeedbackById(userLoggedInDetails.companyId, feedbackId)).build()
     }
 
     @POST
     @Path("/filter")
-    @AllowedRoles(Role.ADMIN,Role.HR)
-    fun getFeedbacksByFilter(@RequestBody feedbackFilter: FeedbackFilter) : Response {
+    @AllowedRoles(Role.ADMIN, Role.HR)
+    fun getFeedbacksByFilter(@RequestBody feedbackFilter: FeedbackFilter): Response {
         val userLoggedInDetails = getUserDetails()
         return Response.ok(feedbackFetcher.getFeedbacksByFilters(userLoggedInDetails.companyId, feedbackFilter)).build()
     }
 
     @GET
     @Path("/{id}/status")
-    fun getFeedbackStatus(@PathParam("id") feedbackId: Long) : Response {
+    fun getFeedbackStatus(@PathParam("id") feedbackId: Long): Response {
         val userLoggedInDetails = getUserDetails()
-        return  Response.ok(feedbackFetcher.getStatus(userLoggedInDetails, feedbackId)).build()
+        return Response.ok(feedbackFetcher.getStatus(userLoggedInDetails, feedbackId)).build()
     }
 
     @PUT
     @Path("/{id}/status/{status}")
     @AllowedRoles(Role.HR)
-    fun updateStatus(@PathParam("id") feedbackId: Long,@PathParam("status") status: String): Response {
+    fun updateStatus(@PathParam("id") feedbackId: Long, @PathParam("status") status: String): Response {
         val userLoggedInDetails = getUserDetails()
-        return Response.ok(feedbackUpdater.updateStatus(userLoggedInDetails.companyId, enumValueOf(status.toString()), feedbackId)).build()
+        return Response.ok(
+            feedbackUpdater.updateStatus(
+                userLoggedInDetails.companyId,
+                enumValueOf(status),
+                feedbackId
+            )
+        ).build()
     }
 
-
+    @POST
+    @Path("/id/response")
+    @AllowedRoles(Role.HR)
+    fun createResponse(@RequestBody responseCreationRequest: ResponseCreationRequest): Response {
+        val userLoggedInDetails = getUserDetails()
+        return Response.ok(feedbackResponseCreator.createResponse(userLoggedInDetails, responseCreationRequest)).build()
+    }
 
 
     private fun getUserDetails(): UserLoggedInDetails {
