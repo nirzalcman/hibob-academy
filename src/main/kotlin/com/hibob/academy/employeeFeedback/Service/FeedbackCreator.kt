a106faf0638d032eec56bbfa4c6e1eff0eaec2f1
Nir Zalcman <nir.zalcman@hibob.io>
2024-09-26 09:42:29 +0300

    Commmit

---
+++ src/main/kotlin/com/hibob/academy/employeeFeedback/Service/FeedbackCreator.kt
@@ -0,0 +1,17 @@
+package com.hibob.academy.employeeFeedback.Service
+
+import com.hibob.academy.employeeFeedback.dao.CreationFeedback
+import com.hibob.academy.employeeFeedback.dao.FeedbackDao
+import com.hibob.academy.employeeFeedback.dao.UserLoggedInDetails
+
+
+class FeedbackCreator(private val feedbackDao: FeedbackDao) {
+    fun createFeedBack(userLoggedInDetails: UserLoggedInDetails, feedback: CreationFeedback): Long {
+        validateFeedback(feedback)
+        return feedbackDao.createFeedback(userLoggedInDetails, feedback)
+    }
+
+    private fun validateFeedback(feedback: CreationFeedback) {
+        require(feedback.content.length in 10..2000) { "Feedback content length must be between 10 and 2000 characters." }
+    }
+}
