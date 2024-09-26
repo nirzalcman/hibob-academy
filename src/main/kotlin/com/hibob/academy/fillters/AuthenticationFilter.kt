3040b01e7d1ed11033963c6464764e9d1e2286a8
Nir Zalcman <nir.zalcman@hibob.io>
2024-09-26 14:21:25 +0300

    remove filter from the pr

--- src/main/kotlin/com/hibob/academy/fillters/AuthenticationFilter.kt
+++ src/main/kotlin/com/hibob/academy/fillters/AuthenticationFilter.kt
@@ -1,4 +1,4 @@
-/*package com.hibob.academy.fillters
+package com.hibob.academy.fillters
 
 import com.hibob.academy.service.SessionService
 import io.jsonwebtoken.Claims
@@ -33,4 +33,3 @@ class AuthenticationFilter(private val sessionService: SessionService) : Contain
 
 }
 
-*/
