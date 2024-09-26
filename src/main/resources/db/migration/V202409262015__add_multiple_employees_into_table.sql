350d23c68f66f556e5d89a748393089f783b20c7
Nir Zalcman <nir.zalcman@hibob.io>
2024-09-26 20:17:20 +0300

    add employees

---
+++ src/main/resources/db/migration/V202409262015__add_multiple_employees_into_table.sql
@@ -0,0 +1,34 @@
+
+INSERT INTO employee (first_name, last_name, role, department, company_id) VALUES
+                                                                               ('Rachel', 'Green', 'ADMIN', 'HR', 1),
+                                                                               ('Ross', 'Geller', 'EMPLOYEE', 'IT', 1),
+                                                                               ('Monica', 'Geller', 'EMPLOYEE', 'Sales', 1),
+                                                                               ('Chandler', 'Bing', 'EMPLOYEE', 'Finance', 1),
+                                                                               ('Joey', 'Tribbiani', 'EMPLOYEE', 'Sales', 1),
+                                                                               ('Phoebe', 'Buffay', 'EMPLOYEE', 'Finance', 1),
+                                                                               ('Gunther', 'Central', 'EMPLOYEE', 'HR', 1),
+                                                                               ('Janice', 'Hosenstein', 'EMPLOYEE', 'Sales', 1),
+                                                                               ('Mike', 'Hannigan', 'EMPLOYEE', 'Finance', 1),
+                                                                               ('David', 'Scientist', 'EMPLOYEE', 'IT', 1),
+                                                                               ('Ben', 'Geller', 'EMPLOYEE', 'Sales', 1),
+                                                                               ('Carol', 'Willick', 'EMPLOYEE', 'IT', 1),
+                                                                               ('Susan', 'Bunch', 'EMPLOYEE', 'HR', 1),
+                                                                               ('Emily', 'Waltham', 'EMPLOYEE', 'Sales', 1),
+                                                                               ('Richard', 'Burke', 'EMPLOYEE', 'Finance', 1);
+
+
+INSERT INTO employee (first_name, last_name, role, department, company_id) VALUES
+                                                                               ('Jerry', 'Seinfeld', 'ADMIN', 'HR', 2),
+                                                                               ('George', 'Costanza', 'EMPLOYEE', 'Finance', 2),
+                                                                               ('Elaine', 'Benes', 'EMPLOYEE', 'Sales', 2),
+                                                                               ('Cosmo', 'Kramer', 'EMPLOYEE', 'IT', 2),
+                                                                               ('Newman', 'Postman', 'EMPLOYEE', 'HR', 2),
+                                                                               ('Frank', 'Costanza', 'EMPLOYEE', 'Finance', 2),
+                                                                               ('Estelle', 'Costanza', 'EMPLOYEE', 'Sales', 2),
+                                                                               ('David', 'Puddy', 'EMPLOYEE', 'Finance', 2),
+                                                                               ('Kenny', 'Bania', 'EMPLOYEE', 'Sales', 2),
+                                                                               ('Jackie', 'Chiles', 'EMPLOYEE', 'HR', 2),
+                                                                               ('Morty', 'Seinfeld', 'EMPLOYEE', 'HR', 2),
+                                                                               ('Helen', 'Seinfeld', 'EMPLOYEE', 'Finance', 2),
+                                                                               ('Uncle', 'Leo', 'EMPLOYEE', 'Sales', 2),
+                                                                               ('Babu', 'Bhatt', 'EMPLOYEE', 'IT', 2);
